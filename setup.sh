710d122fc4a676a8ef9d8fca43ab4b96e71f06e2
CYBER GUARDIANS <cyberguardians404@gmail.com>
2024-05-29 00:06:58 +0530

    update installation of necessary packages, removed unnecessary sudo call

--- setup.sh
+++ setup.sh
@@ -13,6 +13,7 @@ then
 
     sudo apt-get install nmap
     sudo apt-get install python3-pip
+    sudo apt install libnfnetlink-dev libnetfilter-queue-dev
     
      
 elif test "$choice" = "2"
@@ -22,6 +23,7 @@ then
 
     sudo dnf install nmap
     sudo dnf install python3-pip
+    sudo dnf install libnfnetlink-dev libnetfilter-queue-dev
 
 elif test "$choice" = "3"
 then
@@ -30,6 +32,7 @@ then
 
     sudo pacman -S nmap
     sudo pacman -S python3-pip
+    sudo pacman -S libnfnetlink-dev libnetfilter-queue-dev
 
 else
      echo "Invalid choice. Operation cancelled."
@@ -38,17 +41,17 @@ fi
 
 # Install all necessary pip packages
 
-sudo pip3 install gnureadline
-sudo pip3 install colorama
+pip3 install gnureadline
+pip3 install colorama
 
-sudo pip3 install ipaddress
-sudo pip3 install python-nmap
-sudo pip3 install ipinfo
-sudo pip3 install scapy
-sudo pip3 install shodan
-sudo pip3 install python-whois
-sudo pip3 install paramiko
-sudo pip3 install netfilterqueue
+pip3 install ipaddress
+pip3 install python-nmap
+pip3 install ipinfo
+pip3 install scapy
+pip3 install shodan
+pip3 install python-whois
+pip3 install paramiko
+pip3 install netfilterqueue
 
 
 # Add main .py file to the system path (later)
